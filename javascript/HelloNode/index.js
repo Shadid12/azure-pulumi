0490652cc701e193a94afc359e3834d12dd7be1d
shaque <shaque@omers.com>
2021-11-18 18:22:32 -0500

    init

---
+++ javascript/HelloNode/index.js
@@ -0,0 +1,14 @@
+module.exports = async function (context, req) {
+    if (req.query.name || (req.body && req.body.name)) {
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: "My First Test Function: " + (req.query.name || req.body.name)
+        };
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: "Please pass a name here"
+        };
+    }
+};
