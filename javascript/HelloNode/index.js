8aa33cd92ab561866b29f7a704c198c2ce907335
shaque <shaque@omers.com>
2021-11-18 19:39:41 -0500

    added file

--- javascript/HelloNode/index.js
+++ javascript/HelloNode/index.js
@@ -1,10 +1,31 @@
+const faunadb = require('faunadb')
+const q = faunadb.query
+
+var serverClient = new faunadb.Client({ secret: 'fnAEYWP9_zACT50HdgUEvPYiDGv7yoteGaAkhHhQ' })
+
 module.exports = async function (context, req) {
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "My First Test Function: " + (req.query.name || req.body.name)
-        };
+    if (req.query.id) {
+        const id = req.query.id;
+        try {
+            const res = await serverClient.query(
+                q.Get(q.Ref(q.Collection('Book'), id))
+            )
+            console.log('WHEREEE', res);
+            context.res = {
+                body: { book: res.data },
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            };
+        } catch(e) {
+            context.res = {
+                status: 404,
+                body: "Not Found"
+            };
+        }
     }
+
+    
     else {
         context.res = {
             status: 400,
